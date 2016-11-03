d70ecb8824c07ea658dec31b516578077210147e
Mark <Authchirion1>
2016-11-03 22:06:24 +0100

    Changed GuiManager

--- GrusLab/src/game/Main.java
+++ GrusLab/src/game/Main.java
@@ -1,12 +1,9 @@
 package game;
 
-import game.gui.GuiManager;
-
-
 public class Main {
 
 	public static void main(String[] args) {
-			CentralControl.getInstance().startGame();
+		CentralControl.getInstance().startGame();
 	}
 
 
