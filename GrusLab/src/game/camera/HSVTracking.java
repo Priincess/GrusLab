5c2a0280035fd65049649a37382b9ecf69d34a89
Wutti <Wutti@172.22.9.222>
2016-11-03 17:20:26 +0100

    use ObjTracker for methods regarding Camera

---
+++ GrusLab/src/game/camera/HSVTracking.java
@@ -0,0 +1,135 @@
+package game.camera;
+
+
+import java.util.ArrayList;
+
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
+//import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+
+
+public class HSVTracking {
+	
+	private static final Point NOT_VALID = new Point(-1,-1);
+	
+	private Scalar _upperound;
+	
+	private Scalar _lowerbound;
+	
+	Mat _mask;
+	
+	Mat _tracked;
+	
+	public HSVTracking(Scalar upperbound, Scalar lowerbound) {
+		
+		_upperound = upperbound;
+		_lowerbound = lowerbound;
+		_mask = new Mat();
+		
+		
+	}
+	
+	/*returns the found point of the minion - 
+	 * if not valid - it returns a point with -1 and -1
+	 */
+	public Point trackMinion(Mat frame, Minion name) {
+		getMinionBlob(frame, name);
+		return getPosition();
+
+	}
+	
+	/* 
+	 * get the HSV image and cdheck
+	 * if boundaries are in range somewhere in the picture
+	 * 
+	 * */
+	private void getMinionBlob(Mat frame,Minion name){
+
+		//BLuuuur for the colour
+		Imgproc.GaussianBlur(frame, _mask, new Size(11, 11), 3);
+
+		//Magic HSV picture generated
+		Imgproc.cvtColor(frame, _mask, Imgproc.COLOR_BGR2HSV);
+		
+		//Check if boundaries are in range somewhere - returns black white bit img
+		Core.inRange(_mask, _lowerbound, _upperound, _mask);
+		
+		//not sure if erode is needed too much
+		//Imgproc.erode(_mask, _mask, new Mat(), new Point(-1,-1), 1);
+
+		//make sure that one blob is connected as much as possible
+		
+		//yellow minion recognition is working better - so only 2 dilates are needed
+		if(!name.equals(Minion.YELLOW)){
+			
+		Imgproc.dilate(_mask, _mask, new Mat(), new Point(-1,-1), 10);
+		
+		}else{
+			Imgproc.dilate(_mask, _mask, new Mat(), new Point(-1,-1), 2);
+		}
+		
+	//	Highgui.imwrite("dilli" + name + ".jpeg", _mask);
+
+	}
+	
+	
+	/* 
+	 * the Position is determined by searching the largest 
+	 *
+	 * blob which was in range of the boundaries
+	 * */
+	private Point getPosition(){
+		//TODO: PROGRAMMIER DAS SCHÖNER! D:
+		
+		
+		//all contours
+		ArrayList<MatOfPoint> contour = new ArrayList<>();
+		
+		//largestBlob
+		MatOfPoint maxPoint = new MatOfPoint();
+		
+		//the centroid of minion
+		Point center = new Point();
+
+
+		
+		//getAllContours from binary image
+		Imgproc.findContours(_mask, contour, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0,0));
+		
+		double max = 0;
+		double comp = 0;
+		
+		for(MatOfPoint p : contour){
+			comp = Imgproc.contourArea(p, false);
+			if(max < comp){
+				max = comp;
+				maxPoint = p;
+			}
+		}
+
+		//hack for getting centroids out of contours
+		 MatOfPoint2f  centroids = new MatOfPoint2f( maxPoint.toArray() );	 
+		Imgproc.minEnclosingCircle(centroids, center, new float[5]);
+	
+		
+		if (centroids.empty()){
+			return NOT_VALID;
+		}
+		else{
+			return center;
+		}	
+	}
+	
+
+}
---
+++ GrusLab/src/game/camera/ObjTracker.java
@@ -0,0 +1,134 @@
+package game.camera;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.VideoCapture;
+
+public class ObjTracker {
+	
+	
+	//upper & Lower bounds for nice and evil minion
+	private static final Scalar YELLOW_UPPER = new Scalar(1,2);
+	private static final Scalar YELLOW_LOWER = new Scalar(1,2);
+	private static final Scalar EVIL_UPPER = new Scalar(1,2);
+	private static final Scalar EVIL_LOWER = new Scalar(1,2);
+	
+	//ID for webcam to be used
+	private static final int CAM_ID = 1;
+	private static final int NUM_OF_SCALEPOINTS = 4;
+	
+	private static final Point NOT_VALID = new Point(-1,-1);
+	
+    private VideoCapture _capture;
+	private Mat _actualFrame;
+	
+	//variables For trackings
+	HSVTracking _yellowMinion;
+	HSVTracking _evilMinion;
+	
+	
+
+	
+	//actual positions of minions
+	Point _yellowPos;
+	Point _evilPos;
+	
+	
+	public ObjTracker() {
+	
+		//init values
+		_capture = new VideoCapture();
+		_yellowMinion = new HSVTracking(YELLOW_UPPER, YELLOW_LOWER);
+		_evilMinion = new HSVTracking(EVIL_UPPER, EVIL_LOWER);
+		
+		_actualFrame = new Mat();
+		
+		
+		//until now, no minions are tracked - so -1 is not valid value
+		_yellowPos = NOT_VALID;
+		_evilPos = NOT_VALID;
+	}
+	
+	
+	public Point getYellowPos(){
+		return _yellowPos;
+	}
+	
+	public Point getEvilPos(){
+		return _evilPos;
+	}
+	
+	
+	public void startTracking(){
+		
+		_capture.open(CAM_ID);
+		
+		
+		//First time when camrea runs - the first frames are white - so trakcing won't work
+		//so i'll grab the first 1000 frames to make sure to have "clear" PIKTSCHAAA
+		for(int i = 0; i < 1000; i++){
+			_capture.read(_actualFrame);
+		}
+
+		while(_capture.isOpened()){
+			
+			_capture.read(_actualFrame);
+			
+			_yellowPos = _yellowMinion.trackMinion(_actualFrame,  Minion.YELLOW);
+			
+			//to not interfere objectTracking from yellow minion, frame gets closed
+			
+			_evilPos = _evilMinion.trackMinion(_actualFrame.clone(),  Minion.EVIL);
+			
+		}
+		
+		
+	}
+	
+	public boolean stopTracking(){
+		
+		//close camera if not longer tracking
+		if(_capture.isOpened()){
+			_capture.release();
+			return true;
+		}
+		return false;
+	}
+	
+	public Point[] getScalePoints(){
+		Point[] scalePoints = new Point[NUM_OF_SCALEPOINTS];
+		
+		
+		_capture.open(CAM_ID);
+		
+		if(_capture.isOpened()){
+			
+			for(int i = 0; i < 1000; i++){
+				_capture.read(_actualFrame);
+			}
+			
+			Point yellowPoint = _yellowMinion.trackMinion(_actualFrame, Minion.YELLOW);
+			Point evilPoint = _evilMinion.trackMinion(_actualFrame.clone(), Minion.EVIL);
+			
+			int corner = 0;
+			
+			scalePoints[corner] = yellowPoint; 
+			scalePoints[++corner] = new Point(evilPoint.x, yellowPoint.y);
+			scalePoints[++corner] = new Point(yellowPoint.x, evilPoint.y);
+			scalePoints[++corner] = evilPoint;
+			
+			_capture.release();
+			
+			return scalePoints;
+		}
+		
+		return new Point[]{NOT_VALID, NOT_VALID, NOT_VALID, NOT_VALID};
+	}
+	
+
+	
+	
+	
+	
+}
