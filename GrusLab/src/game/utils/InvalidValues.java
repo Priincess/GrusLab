df89f78304cdfddf7c744d636d838f3fa54e4efe
Mark <Authchirion1>
2016-12-05 14:21:25 +0100

    Utils

---
+++ GrusLab/src/game/utils/InvalidValues.java
@@ -0,0 +1,12 @@
+package game.utils;
+
+import org.opencv.core.Point;
+
+/**
+ * Created by Mark Mauerhofer on 05.12.2016.
+ */
+public class InvalidValues {
+    private static final Point INVALID_CAMERA_POINT = null;
+    //private static final game.gameboard.Point INVALID_GAMEBOARD_POINT = null;
+
+}
